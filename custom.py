90c4bb7d2a526c21084f2289e761b00daebf8ee3
Kira <29959059+UnknownSkeletalRemains@users.noreply.github.com>
2026-07-15 14:56:16 +0300

    Update custom.py

--- custom.py
+++ custom.py
@@ -3,25 +3,24 @@ module_jolt_physics_enabled = "no"
 module_bullet_enabled = "no"
 
 
-# 3D systems
+# 3D
 module_navigation_enabled = "no"
 module_gridmap_enabled = "no"
 module_csg_enabled = "no"
+module_particles_enabled = "no"
+module_fbx_enabled = "no"
+module_gltf_enabled = "no"
 
 
 # XR
 module_openxr_enabled = "no"
 
 
-# Multiplayer / networking
+# Networking
 module_multiplayer_enabled = "no"
 module_webrtc_enabled = "no"
 module_websocket_enabled = "no"
 
 
-# Importers
-module_fbx_enabled = "no"
-
-
-# Game systems
-module_particles_enabled = "no"
+# Other
+module_voxel_enabled = "no"

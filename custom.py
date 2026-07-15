be76c2ceaad7b4beb92911adcf0260bf7bbca117
Kira <29959059+UnknownSkeletalRemains@users.noreply.github.com>
2026-07-15 14:08:39 +0300

    Add configuration options for various modules

--- custom.py
+++ custom.py
@@ -1,25 +1,32 @@
-
-
+# Physics
 module_jolt_physics_enabled = "no"
 module_bullet_enabled = "no"
 
 
+# 3D systems
 module_navigation_enabled = "no"
 module_gridmap_enabled = "no"
 module_csg_enabled = "no"
 
 
-
+# XR
 module_openxr_enabled = "no"
 
 
+# Multiplayer / networking
 module_multiplayer_enabled = "no"
 module_webrtc_enabled = "no"
 module_websocket_enabled = "no"
 
 
-
+# Importers
 module_fbx_enabled = "no"
 
 
+# Game systems
 module_particles_enabled = "no"
+
+
+# Build dependencies
+accesskit = "no"
+d3d12 = "no"

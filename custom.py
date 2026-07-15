36bbf6009fd0f3f9728655f4d5c3a3d1e9793c9c
Kira <29959059+UnknownSkeletalRemains@users.noreply.github.com>
2026-07-15 13:56:33 +0300

    Update custom.py

--- custom.py
+++ custom.py
@@ -1,17 +1,25 @@
-module_3d_enabled = "no"
-module_gridmap_enabled = "no"
-module_csg_enabled = "no"
-module_particles_enabled = "no"
-module_environment_enabled = "no"
+
 
 module_jolt_physics_enabled = "no"
 module_bullet_enabled = "no"
 
+
 module_navigation_enabled = "no"
+module_gridmap_enabled = "no"
+module_csg_enabled = "no"
+
+
 
 module_openxr_enabled = "no"
 
+
+module_multiplayer_enabled = "no"
 module_webrtc_enabled = "no"
 module_websocket_enabled = "no"
 
-module_multiplayer_enabled = "no"
+
+
+module_fbx_enabled = "no"
+
+
+module_particles_enabled = "no"
